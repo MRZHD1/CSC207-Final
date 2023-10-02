7f04db655e2f60c5486612eeb20dea52e48324c2
Omer Ali <75752223+MRZHD1@users.noreply.github.com>
2023-10-01 22:10:34 -0400

    Create api_test.java
    Added test code.

---
+++ api_test.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.net.*;
+
+
+
+public class main {
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        URL url = new URL("https://api.openweathermap.org/data/2.5/weather?lat=0&lon=0&appid=eb46926a4e98e257c2c14f2b9fcd1899");
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+
+
+        int code = connection.getResponseCode();
+        System.out.println(code); // Returns status code if request went through.
+    }
+}
