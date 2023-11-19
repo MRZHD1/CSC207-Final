c7e09d82a0cf4e18f21c84870d640f050f7301bb
Suhani D. <suhanidabhi24@gmail.com>
2023-11-19 13:26:02 -0500

    initializing a user

--- src/entity/CommonUser.java
+++ src/entity/CommonUser.java
@@ -1,4 +1,43 @@
 package entity;
 
-public class CommonUser {
+import java.time.LocalDateTime;
+
+public class CommonUser implements User{
+
+    private final String userName;
+    private final String password;
+
+    private final LocalDateTime creationTime;
+
+    /**
+     * @param userName
+     * @param password
+     * @param creationTime
+     */
+    public CommonUser(String userName, String password, LocalDateTime creationTime) {
+        this.userName = userName;
+        this.password = password;
+
+        this.creationTime = creationTime;
+    }
+    @Override
+    public String getUserName() {
+        return userName;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+
+
+    @Override
+    public LocalDateTime getCreationTime() {
+        return creationTime;
+        }
+    }
+
+
+
+
+
 }
