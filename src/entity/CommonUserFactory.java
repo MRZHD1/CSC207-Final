482701e18a0fc6e53b687845ee28724066a4da87
Suhani D. <suhanidabhi24@gmail.com>
2023-11-19 13:24:37 -0500

    returns users

--- src/entity/CommonUserFactory.java
+++ src/entity/CommonUserFactory.java
@@ -1,4 +1,12 @@
 package entity;
 
-public class CommonUserFactory {
+import java.time.LocalDateTime;
+
+public class CommonUserFactory implements entity.UserFactory {
+
+    @Override
+    public entity.User create(String userName, String password, LocalDateTime time) {
+        return new entity.CommonUser(userName, password, time);
+    }
 }
+
