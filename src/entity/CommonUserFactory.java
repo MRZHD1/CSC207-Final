036fe4a011ed104328d610b037b810c177515acb
Suhani D. <suhanidabhi24@gmail.com>
2023-11-20 10:20:44 -0500

    initializing a user

--- src/entity/CommonUserFactory.java
+++ src/entity/CommonUserFactory.java
@@ -5,8 +5,9 @@ import java.time.LocalDateTime;
 public class CommonUserFactory implements entity.UserFactory {
 
     @Override
-    public entity.User create(String userName, String password, LocalDateTime time) {
-        return new entity.CommonUser(userName, password, time);
+    public entity.User create(String name, String password, LocalDateTime ltd) {
+        return new CommonUser(name, password, ltd);
     }
+
 }
 
