af76782efd2da35ea872d63475fd0760774dc43a
akhilp2003 <akhil.pindiprolu@mail.utoronto.ca>
2023-12-06 16:02:46 -0500

    made results presenter

--- src/interface_adapter/results/ResultsPresenter.java
+++ src/interface_adapter/results/ResultsPresenter.java
@@ -1,4 +1,47 @@
-package interface_adapter.results;
-
-public class ResultsPresenter {
-}
+//package interface_adapter.results;
+//import interface_adapter.ViewManagerModel;
+//import interface_adapter.search.SearchState;
+//import interface_adapter.search.SearchViewModel;
+//import interface_adapter.specific.SpecificState;
+//import interface_adapter.specific.SpecificViewModel;
+//import use_case.results.ResultsOutputBoundary;
+//import use_case.results.ResultsOutputData;
+//import use_case.search.SearchOutputData;
+//
+//public class ResultsPresenter implements ResultsOutputBoundary{
+//
+//
+//    private final ResultsViewModel resultsViewModel;
+//    private final SpecificViewModel specificViewModel;
+//    private ViewManagerModel viewManagerModel;
+//
+//
+//    public ResultsPresenter(ResultsViewModel resultsViewModel,
+//                           SpecificViewModel specificViewModel,
+//                           ViewManagerModel viewManagerModel) {
+//        this.resultsViewModel = resultsViewModel;
+//        this.specificViewModel = specificViewModel;
+//        this.viewManagerModel = viewManagerModel;
+//    }
+//
+//    @Override
+//    public void prepareSuccessView(ResultsOutputData response) {
+//
+//        SpecificState specificState = specificViewModel.getState();
+//        specificState.setSpecific(response.getSpecific());
+//        this.specificViewModel.setState(specificState);
+//        this.specificViewModel.firePropertyChanged();
+//
+//        this.viewManagerModel.setActiveView(specificViewModel.getViewName());
+//        this.viewManagerModel.firePropertyChanged();
+//    }
+//
+//    @Override
+//    public void prepareFailView(String error) {
+//        ResultsState resultsState = resultsViewModel.getState();
+//        resultsState.setResultsError(error);
+//        this.resultsViewModel.setState(resultsState);
+//        this.resultsViewModel.firePropertyChanged();
+//    }
+//
+//}
