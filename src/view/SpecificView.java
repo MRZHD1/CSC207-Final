7140c25285eb2411ebfad603dc5999a8837d4bcb
akhilp2003 <akhil.pindiprolu@mail.utoronto.ca>
2023-12-06 23:28:13 -0500

    added to specific view and made it nice

--- src/view/SpecificView.java
+++ src/view/SpecificView.java
@@ -1,20 +1,18 @@
 package view;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.util.ArrayList;
-import javax.swing.ButtonGroup;
+import javax.swing.border.EmptyBorder;
 
 import entity.DetailedPlace;
-import interface_adapter.results.ResultsState;
 import interface_adapter.specific.SpecificState;
 import interface_adapter.specific.SpecificViewModel;
 
-public class SpecificView extends JPanel implements ActionListener, PropertyChangeListener{
-
+public class SpecificView extends JPanel implements ActionListener, PropertyChangeListener {
 
     public final String viewName = "specific";
     private final SpecificViewModel specificViewModel;
@@ -27,19 +25,23 @@ public class SpecificView extends JPanel implements ActionListener, PropertyChan
 
         quitButton = new JButton("Quit");
         quitButton.addActionListener(this);
+        quitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Add some space between the detailedPlace and the quitButton
+        quitButton.setBorder(new EmptyBorder(700, 0, 0, 0));
 
         add(quitButton);
 
-        JLabel title = new JLabel("More Details");
+        JLabel title = new JLabel("Website");
         title.setHorizontalAlignment(JLabel.CENTER);
-        title.setBorder(BorderFactory.createEmptyBorder(0, 100, 50, 0));
-
+        add(title);
     }
+
     @Override
     public void actionPerformed(ActionEvent evt) {
         System.out.println("Click " + evt.getActionCommand());
 
-        if (evt.getSource() == quitButton){
+        if (evt.getSource() == quitButton) {
             System.exit(0);
         }
     }
@@ -49,15 +51,23 @@ public class SpecificView extends JPanel implements ActionListener, PropertyChan
         SpecificState state = (SpecificState) evt.getNewValue();
         DetailedPlace detailedPlace = state.getDetailedPlace();
 
+        // Clear existing components
+        removeAll();
 
+        JLabel name = new JLabel(detailedPlace.getName());
+        JLabel website = new JLabel(detailedPlace.getWebsite());
+        JLabel address = new JLabel(detailedPlace.getAddress());
 
+        JPanel textPanel = new JPanel();
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(name, BorderLayout.NORTH);  // Align name to the top
+        textPanel.add(website, BorderLayout.CENTER);  // Center website
+        textPanel.add(address, BorderLayout.SOUTH);
 
+        add(textPanel);
+
+        // Revalidate and repaint the panel
+        revalidate();
+        repaint();
     }
-
-
 }
-
-
-
-
-
